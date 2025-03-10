a453b695c32ded4a0bb7e3101406d955f6e69ae5
lcky <lcky>
2025-03-11 00:05:17 +0100

    bug fix

--- services/trading_view.py
+++ services/trading_view.py
@@ -54,7 +54,7 @@ class TradingView:
     @staticmethod
     def chromeOptions():
         options = webdriver.ChromeOptions()
-        options.add_argument('--headless')
+        #options.add_argument('--headless')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
         user_data_dir = tempfile.mkdtemp()
